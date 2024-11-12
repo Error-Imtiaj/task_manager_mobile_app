1b2c16ad71d1aa0e6f9ce36106f327f2159fb8ee
Error-Imtiaj <imtiaj0044@gmail.com>
2024-11-13 02:00:42 +0600

    update

--- lib/UI/screens/set_password_screen.dart
+++ lib/UI/screens/set_password_screen.dart
@@ -1,13 +1,11 @@
-
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:task_manager_mobile_app/UI/controller/set_password_controller.dart';
 import 'package:task_manager_mobile_app/UI/screens/login_screen.dart';
 import 'package:task_manager_mobile_app/UI/utils/colors.dart';
-import 'package:task_manager_mobile_app/UI/utils/urls.dart';
 import 'package:task_manager_mobile_app/UI/widgets/custom_button.dart';
 import 'package:task_manager_mobile_app/UI/widgets/snack_bar.dart';
 import 'package:task_manager_mobile_app/UI/widgets/textfield_widget.dart';
-import 'package:task_manager_mobile_app/data/model/network_response.dart';
-import 'package:task_manager_mobile_app/data/services/network_caller.dart';
 
 class SetPasswordScreen extends StatefulWidget {
   final String email;
@@ -22,7 +20,7 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
   final TextEditingController _passController = TextEditingController();
   final TextEditingController _confirmPassController = TextEditingController();
   final GlobalKey<FormState> _formkey = GlobalKey<FormState>();
-  bool _isLoading = false;
+  final SetPasswordController controller = Get.find<SetPasswordController>();
 
   @override
   void dispose() {
@@ -46,133 +44,122 @@ class _SetPasswordScreenState extends State<SetPasswordScreen> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(40.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              "Set Your Password",
-              style: Theme.of(context).textTheme.displayMedium?.copyWith(
-                  fontWeight: FontWeight.bold,
-                  color: ColorsUtils.primaryColor,
-                  fontSize: 32),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
+        child: Form(
+          key: _formkey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "Set Your Password",
+                style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      color: ColorsUtils.primaryColor,
+                      fontSize: 32,
+                    ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
 
-            Text(
-              "Minimum number of password should be 6 digits",
-              style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                    fontWeight: FontWeight.w900,
-                    color: Colors.grey,
-                    fontSize: 14,
-                  ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
+              Text(
+                "Minimum number of password should be 6 digits",
+                style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                      fontWeight: FontWeight.w900,
+                      color: Colors.grey,
+                      fontSize: 14,
+                    ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
 
-            // PASSWORD FIELD
-            Textfieldwidget(
-              fillColor: ColorsUtils.fieldPrimaryColor,
-              controller: _passController,
-              hintText: "Password",
-              validatorFunction: (p0) => _validator(p0),
-            ),
-            const SizedBox(
-              height: 10,
-            ),
+              // PASSWORD FIELD
+              Textfieldwidget(
+                fillColor: ColorsUtils.fieldPrimaryColor,
+                controller: _passController,
+                hintText: "Password",
+                validatorFunction: (p0) => _validator(p0),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
 
-            // CONFIRM PASSWORD FIELD
-            Form(
-              key: _formkey,
-              child: Textfieldwidget(
+              // CONFIRM PASSWORD FIELD
+              Textfieldwidget(
                 fillColor: ColorsUtils.fieldPrimaryColor,
                 controller: _confirmPassController,
                 hintText: "Confirm Password",
                 validatorFunction: (p0) => _validator2(p0),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            // BUTTON SET PASSWORD
-            Visibility(
-              visible: !_isLoading,
-              replacement: const Center(
-                child: CircularProgressIndicator(),
+              const SizedBox(
+                height: 20,
               ),
-              child: Custombutton(
-                ButtonName: "Set Password",
-                ontap: () {
-                  if (_formkey.currentState!.validate()) {
-                    _setPassword();
-                  }
+              // BUTTON SET PASSWORD
+              GetBuilder<SetPasswordController>(
+                init: controller,
+                initState: (_) {},
+                builder: (controller) {
+                  // Change `context` to `controller`
+                  return Visibility(
+                    visible: !(controller.isLoading ??
+                        false), // Access isLoading from the controller
+                    replacement: const Center(
+                      child: CircularProgressIndicator(),
+                    ),
+                    child: Custombutton(
+                      ButtonName: "Set Password",
+                      ontap: () {
+                        if (_formkey.currentState!.validate()) {
+                          if (_passController.text ==
+                              _confirmPassController.text) {
+                            _setPassword();
+                          } else {
+                            showSnackBar(context, "Passwords do not match");
+                          }
+                        }
+                      },
+                    ),
+                  );
                 },
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future _setPassword() async {
-    try {
-      _isLoading = true;
-      setState(() {});
-      Map<String, dynamic> body = {
-        "email": widget.email,
-        "OTP": widget.otp,
-        "password": _passController.text,
-      };
-      NetworkModel setPasswordResponse = await NetworkCaller.postRequest(
-        url: Urls.setPassUrl,
-        body: body,
-      );
-      _isLoading = false;
-      setState(() {});
-      if (setPasswordResponse.statusCode == 200) {
-        showSnackBar(context, setPasswordResponse.message);
-        _navigateToSignInPage();
-      } else {
-        showSnackBar(context, setPasswordResponse.errorMessage);
-      }
-    } catch (e) {
-      _isLoading = false;
-      setState(() {});
-      showSnackBar(context, e.toString());
+    final bool setResponse = await controller.setPassword(
+        widget.email, widget.otp, _passController.text);
+    if (setResponse) {
+      showSnackBar(context, controller.messsage!);
+      _navigateToSignInPage();
+    } else {
+      showSnackBar(context, controller.messsage!);
     }
   }
 
   void _navigateToSignInPage() {
-    Navigator.push(context, MaterialPageRoute(builder: (context) {
-      return const LoginScreen();
-    }));
+    Get.toNamed(LoginScreen.text);
   }
 
-  _validator(
-    String? value,
-  ) {
+  String? _validator(String? value) {
     if (value!.isEmpty) {
       return 'Field must not be empty';
-    } else if (value.length <= 8) {
-      return 'Input must be more than 8 character';
+    } else if (value.length <= 5) {
+      return 'Input must be more than 5 characters';
     } else {
       return null;
     }
   }
 
-  _validator2(
-    String? value,
-  ) {
-    if (value!.isEmpty) {
+  String? _validator2(String? value) {
+    if (value == null || value.isEmpty) {
       return 'Field must not be empty';
-    } else if (value.length <= 8) {
-      return 'Input must be more than 8 character';
-    } else if (_passController.text != _confirmPassController.text) {
-      return 'Password do not match';
+    } else if (value.length <= 5) {
+      return 'Input must be more than 5 characters';
     } else {
       return null;
     }
